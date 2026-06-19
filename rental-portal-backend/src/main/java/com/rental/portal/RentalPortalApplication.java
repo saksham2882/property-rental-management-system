b886c80d56bded250895feb3344229d89a8427a6
Saksham Agrahari <agrahari0899@gmail.com>
2026-06-19 18:13:11 +0530

    initial commit

---
+++ rental-portal-backend/src/main/java/com/rental/portal/RentalPortalApplication.java
@@ -0,0 +1,12 @@
+package com.rental.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalPortalApplication.class, args);
+	}
+}
