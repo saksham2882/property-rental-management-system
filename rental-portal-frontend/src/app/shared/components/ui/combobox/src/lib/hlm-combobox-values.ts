abb3380ab1175ad67c2f98a96609055b23bf4e2e
Saksham Agrahari <agrahari0899@gmail.com>
2026-06-25 11:20:38 +0530

    fix(frontend): remove spartan ui which causes issues

--- rental-portal-frontend/src/app/shared/components/ui/combobox/src/lib/hlm-combobox-values.ts
+++
@@ -1,5 +0,0 @@
-import { Directive } from '@angular/core';
-import { BrnComboboxValues } from '@spartan-ng/brain/combobox';
-
-@Directive({ selector: '[hlmComboboxValues]', hostDirectives: [BrnComboboxValues] })
-export class HlmComboboxValues {}
--- rental-portal-frontend/src/app/shared/components/ui/select/src/lib/hlm-select-values-content.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from '@angular/core';
-import { classes } from '@spartan-ng/helm/utils';
-
-@Directive({ selector: '[hlmSelectValuesContent],hlm-select-values-content' })
-export class HlmSelectValuesContent {
-  constructor() {
-    classes(() => 'gap-2 flex');
-  }
-}
