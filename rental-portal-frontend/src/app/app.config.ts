d66c3a977ab449050b7f72f38bfd3b0eeb976b66
Saksham Agrahari <agrahari0899@gmail.com>
2026-07-06 17:11:18 +0530

    fix(frontend): rename file

--- rental-portal-frontend/src/app/app.config.ts
+++ rental-portal-frontend/src/app/app.config.ts
@@ -8,7 +8,7 @@ import { GlobalErrorHandler } from './core/global/global-error-handler';
 import { appReducers, appEffects } from './store';
 import { routes } from './app.routes';
 import { cacheInterceptor } from './core/interceptor/cache-interceptor';
-import { loaderInterceptor } from './core/interceptor/loader.interceptor';
+import { loaderInterceptor } from './core/interceptor/loader-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
