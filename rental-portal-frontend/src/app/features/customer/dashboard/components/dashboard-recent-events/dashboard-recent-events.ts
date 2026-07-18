99cbb9584dc82c308800c8e620927015794010e6
Saksham Agrahari <agrahari0899@gmail.com>
2026-07-19 00:01:06 +0530

    feat(frontend): add dashboard for customer and fix css in its components

--- rental-portal-frontend/src/app/features/customer/dashboard/components/dashboard-recent-events/dashboard-recent-events.ts
+++ rental-portal-frontend/src/app/features/customer/dashboard/components/dashboard-recent-events/dashboard-recent-events.ts
@@ -2,15 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { IconComponent } from '../../../../../shared/components/icon/icon';
-
-export interface ActivityItem {
-  title: string;
-  desc: string;
-  time: string;
-  type: 'success' | 'warning' | 'info' | 'danger';
-  icon: string;
-  date: Date;
-}
+import { ActivityItem } from '../../services/dashboard-service';
 
 @Component({
   selector: 'dashboard-recent-events',
@@ -19,6 +11,6 @@ export interface ActivityItem {
   styleUrl: './dashboard-recent-events.css'
 })
 export class DashboardRecentEventsComponent {
-  
+
   @Input() activities: ActivityItem[] = [];
 }
